cb876dbd58dddaddc5435aee9c59d9e68105d6fa
panagiotis2011 <sch@sch.gr>
2012-01-07 22:17:44 +0200

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 EapApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+
+  root :to => 'pages#home'
 end
